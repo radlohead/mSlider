d06754a802ce0d9f36217d959c85345e7adf3d93
radlohead <radlohead@naver.com>
2017-03-20 22:45:10 +0900

    17-3-20 handleLeftClick 추가 및 리펙토링

--- mSlider.js
+++ mSlider.js
@@ -11,6 +11,7 @@ class mSlider {
         this.listStyle = null;
         this.listWidth = 0;
         this.moveItem = 0;
+        this.status = true;
         this.btns = this.wrapper.querySelector('.mSlider-controls');
         this.nextBtn = this.btns.querySelector('.nextBtn');
         this.prevBtn = this.btns.querySelector('.prevBtn');
@@ -40,61 +41,69 @@ class mSlider {
     //rightBtn click event
     handleRightClick(){
         const RightClick = ()=> {
-            this.list.map((v,i)=> {
-                //노출되지않던 리스트가 보여지는 상태로 바뀜 처음 리스트만
+            if(this.status){
+                this.status = false;
+                this.list.map((v,i)=> {
+                    //노출되지않던 리스트가 보여지는 상태로 바뀜 처음 리스트만
+                        const objThis = this;
+                        this.moveItem = 0;
+                        const timer = setInterval(frame, 1);
+
+                        function frame() {
+                            if (objThis.moveItem == -objThis.listWidth) {
+                                clearInterval(timer);
+
+                                //노출된 리스트가 스크린밖으로 사라지는 단계 완전히 포커싱이 끝나면 리스트 끝으로 이동한다.
+                                if(parseInt(objThis.list[i].style.left) < 0) {
+                                    objThis.list[i].style.left = parseInt(objThis.listWidth) * (objThis.list.length-1) + 'px';
+                                }
+                                objThis.status = true;
+                            } else {
+                                //오른쪽에 노출되지 않던 리스트가 화면으로 들어옴
+                                objThis.moveItem--;
+                                for (let j = 0; j < objThis.list.length; j++) {
+                                    objThis.list[j].style.left = parseInt(objThis.list[j].style.left) - 1 + 'px';
+                                }
+                            }
+                        };
+                });
+            }
+        };
+        this.nextBtn.addEventListener("click", function () {
+            RightClick();
+        }, false);
+    }
+    handleLeftClick(){
+        const LeftClick = ()=> {
+            if(this.status){
+                this.status = false;
+                this.list.map((v,i)=> {
+                    //노출되지않던 리스트가 보여지는 상태로 바뀜 처음 리스트만
                     const objThis = this;
                     this.moveItem = 0;
-                    const timer = setInterval(frame, 5);
+                    const timer = setInterval(frame, 1);
+
+                    //오른쪽 끝에 있던 리스트가 화면 왼쪽으로 이동함
+                    objThis.list.map((v,i)=> {
+                        if(parseInt(objThis.list[i].style.left) == objThis.listWidth * (objThis.list.length-1)){
+                            objThis.list[i].style.left = -objThis.listWidth + 'px';
+                        }
+                    });
 
                     function frame() {
                         if (objThis.moveItem == -objThis.listWidth) {
                             clearInterval(timer);
-
-                            //노출된 리스트가 스크린밖으로 사라지는 단계 완전히 포커싱이 끝나면 리스트 끝으로 이동한다.
-                            if(parseInt(objThis.list[i].style.left) < 0) {
-                                objThis.list[i].style.left = parseInt(objThis.listWidth) * (objThis.list.length-1) + 'px';
-                            }
+                            objThis.status = true;
                         } else {
-                            //오른쪽에 노출되지 않던 리스트가 화면으로 들어옴
+                            //리스트가 오른쪽으로 넓이값 만큼 이동
                             objThis.moveItem--;
                             for (let j = 0; j < objThis.list.length; j++) {
-                                objThis.list[j].style.left = parseInt(objThis.list[j].style.left) - 1 + 'px';
+                                objThis.list[j].style.left = parseInt(objThis.list[j].style.left) + 1 + 'px';
                             }
                         }
                     };
-            });
-        };
-        this.nextBtn.addEventListener("click", function () {
-            RightClick();
-        }, true);
-    }
-    handleLeftClick(){
-        const LeftClick = ()=> {
-            this.list.map((v,i)=> {
-                //노출되지않던 리스트가 보여지는 상태로 바뀜 처음 리스트만
-                const objThis = this;
-                this.moveItem = 0;
-                const timer = setInterval(frame, 5);
-
-                //오른쪽 끝에 있던 리스트가 화면 왼쪽으로 이동함
-                objThis.list.map((v,i)=> {
-                    if(parseInt(objThis.list[i].style.left) == objThis.listWidth * (objThis.list.length-1)){
-                        objThis.list[i].style.left = -objThis.listWidth + 'px';
-                    }
                 });
-
-                function frame() {
-                    if (objThis.moveItem == -objThis.listWidth) {
-                        clearInterval(timer);
-                    } else {
-                        //리스트가 오른쪽으로 넓이값 만큼 이동
-                        objThis.moveItem--;
-                        for (let j = 0; j < objThis.list.length; j++) {
-                            objThis.list[j].style.left = parseInt(objThis.list[j].style.left) + 1 + 'px';
-                        }
-                    }
-                };
-            });
+            }
         };
         this.prevBtn.addEventListener("click", function () {
             LeftClick();
