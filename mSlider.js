280809385337922280e27a647622f3014373feb0
radlohead <radlohead@naver.com>
2017-03-23 01:00:57 +0900

    17-3-22 리펙토링

--- mSlider.js
+++ mSlider.js
@@ -15,6 +15,15 @@ class mSlider {
         this.btns = this.wrapper.querySelector('.mSlider-controls');
         this.nextBtn = this.btns.querySelector('.nextBtn');
         this.prevBtn = this.btns.querySelector('.prevBtn');
+
+        //bullet-icon
+        this.bulletIcon = this.doc.querySelectorAll('.bullet-icon li');
+        this.bullet = ()=> {
+            this.list.map((v, i)=> {
+                this.bulletIcon[i].classList.remove('on');
+                if(parseInt(v.style.left) == 0) this.bulletIcon[i].classList.add('on');
+            });
+        }
     }
     //시작시 초기화
     init(){
@@ -31,7 +40,7 @@ class mSlider {
             this.list.map((v, i)=> {
                 this.listStyle = window.getComputedStyle(this.list[i]);
                 this.listWidth = parseInt(this.listStyle.width);
-                this.list[i].style.left = parseInt(this.listStyle.width) * i + 'px';
+                this.list[i].style.left = this.listWidth * i + 'px';
             });
         };
         listLeft();
@@ -47,6 +56,7 @@ class mSlider {
                     //노출되지않던 리스트가 보여지는 상태로 바뀜 처음 리스트만
                         const objThis = this;
                         this.moveItem = 0;
+
                         const timer = setInterval(frame, 1);
 
                         function frame() {
@@ -56,6 +66,9 @@ class mSlider {
                                 //노출된 리스트가 스크린밖으로 사라지는 단계 완전히 포커싱이 끝나면 리스트 끝으로 이동한다.
                                 if(parseInt(objThis.list[i].style.left) < 0) {
                                     objThis.list[i].style.left = parseInt(objThis.listWidth) * (objThis.list.length-1) + 'px';
+
+                                    //bullet-icon class 삽입
+                                    objThis.bullet();
                                 }
                                 objThis.status = true;
                             } else {
@@ -69,15 +82,15 @@ class mSlider {
                 });
             }
         };
-        this.nextBtn.addEventListener("click", function () {
+        this.nextBtn.addEventListener("click", ()=> {
             RightClick();
-        }, false);
+        }, true);
     }
     handleLeftClick(){
         const LeftClick = ()=> {
             if(this.status){
                 this.status = false;
-                this.list.map((v,i)=> {
+                this.list.map(()=> {
                     //노출되지않던 리스트가 보여지는 상태로 바뀜 처음 리스트만
                     const objThis = this;
                     this.moveItem = 0;
@@ -94,6 +107,8 @@ class mSlider {
                         if (objThis.moveItem == -objThis.listWidth) {
                             clearInterval(timer);
                             objThis.status = true;
+                            //bullet-icon class 삽입
+                            objThis.bullet();
                         } else {
                             //리스트가 오른쪽으로 넓이값 만큼 이동
                             objThis.moveItem--;
@@ -105,7 +120,7 @@ class mSlider {
                 });
             }
         };
-        this.prevBtn.addEventListener("click", function () {
+        this.prevBtn.addEventListener("click", ()=> {
             LeftClick();
         }, true);
     }
